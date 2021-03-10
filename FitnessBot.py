b41d29e74011b59423eb5d192364253a00f5157f
Zhaoyuan Yu <zyu247@uwo.ca>
2021-03-10 15:25:42 -0500

    reformat

---
+++ FitnessBot.py
@@ -0,0 +1,11 @@
+import ChatBot_GUI
+
+
+def main():
+    print("running chat bot")
+    ChatBot_GUI.root.mainloop()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
